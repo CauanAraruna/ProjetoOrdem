583aa408bfbef3e3e73fb79832b73276f289d26d
CauanAraruna <cauan.araruna@sptech.school>
2024-05-23 16:08:13 -0300

    Comeco da aba comunidade e leve alteracoes

--- individual/web-data-viz/src/database/script-tabelas.sql
+++ individual/web-data-viz/src/database/script-tabelas.sql
@@ -6,46 +6,69 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
 
-USE aquatech;
+create database projetoOrdem;
+use projetoOrdem;
+
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+    elemento varchar(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+select * from usuario;
+select * from mensagemHome;
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+create table mensagemHome (
+idMensagem int primary key auto_increment,
+mensagem varchar(250));
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+select * from mensagemHome;
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+-- CREATE DATABASE aquatech;
+
+-- USE aquatech;
+
+-- CREATE TABLE usuario (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	nome VARCHAR(50),
+-- 	email VARCHAR(50),
+-- 	senha VARCHAR(50)
+-- );
+
+ CREATE TABLE aviso (
+ 	id INT PRIMARY KEY AUTO_INCREMENT,
+ 	titulo VARCHAR(100),
+ 	descricao VARCHAR(150),
+ 	fk_usuario INT,
+ 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+ );
+
+-- create table aquario (
+-- /* em nossa regra de negócio, um aquario tem apenas um sensor */
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	descricao VARCHAR(300),
+-- 	fk_empresa INT,
+-- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+-- );
+
+-- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
+
+-- insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
+-- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
